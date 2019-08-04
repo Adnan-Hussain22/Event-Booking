9ac44a328ae9eb0e0e01cee88c987f26afe0521f
Adnan-Hussain22 <adnanrajput42@gmail.com>
2019-08-04 16:19:09 +0500

    resolver populators

---
+++ graphql/resolver/populators.js
@@ -0,0 +1,61 @@
+const { date: { dateToString } } = require('../../helpers');
+const { User, Event } = require('../../models');
+const events = async (eventIds) => {
+	try {
+		const events = await Event.find({ _id: { $in: eventIds } });
+		return events.map((event) => populateEvent(event));
+	} catch (err) {
+		throw err;
+	}
+};
+
+const singleEvent = async (eventId) => {
+	try {
+		const event = await Event.findById(eventId);
+		return populateEvent(event);
+	} catch (err) {
+		throw err;
+	}
+};
+
+const user = async (userId) => {
+	try {
+		const user = await User.findById(userId);
+		return {
+			...user._doc,
+			_id: user.id,
+			password: null,
+			createdEvents: events.bind(this, user._doc.createdEvents)
+		};
+	} catch (err) {
+		throw err;
+	}
+};
+
+const populateEvent = (event) => {
+	return {
+		...event._doc,
+		_id: event.id,
+		date: dateToString(event._doc.date),
+		creator: user.bind(this, event.creator)
+	};
+};
+
+const populateBooking = (booking) => {
+	return {
+		...booking._doc,
+		_id: booking.id,
+		user: user.bind(this, booking._doc.user),
+		event: singleEvent.bind(this, booking._doc.event),
+		createdAt: dateToString(booking._doc.createdAt),
+		updatedAt: dateToString(booking._doc.updatedAt)
+	};
+};
+
+module.exports = {
+	user,
+	events,
+	singleEvent,
+	populateBooking,
+	populateEvent
+};
